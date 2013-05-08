e678c7840e68dfad02b71cbc53d6e2fed876bf4d
Urpo Lankinen <wwwwolf@iki.fi>
2013-05-08 19:15:08 +0300

    Now it camps!

--- antilaconia.rb
+++ antilaconia.rb
@@ -1,39 +1,60 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
-gem 'activerecord'
+gem     'camping'
 
-require 'sqlite3'
-require 'active_record'
+require 'camping'
 
-# Database connection
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'test.db'
-)
+Camping.goes :Antilaconia
 
-# Data schema
-class User < ActiveRecord::Base
-  has_many :posts
-  validates_uniqueness_of :name
+module Antilaconia::Models
+  class User < Base
+    has_many :posts
+    validates_uniqueness_of :username
+  end
+  class Post < Base
+    belongs_to :user
+  end
+  class BasicFields < V 1.0
+    def self.up
+      create_table User.table_name do |t|
+        t.string :username, :limit => 40
+        t.string :password_hash, :limit => 200
+        t.string :password_salt, :limit => 200
+      end
+      create_table Post.table_name do |t|
+        t.references :user
+        t.string :text, :limit => 140
+        t.timestamps
+      end
+    end    
+    def self.down
+      drop_table User.table_name
+      drop_table Post.table_name
+    end
+  end
+  def Antilaconia.create
+    Antilaconia::Models.create_schema
+  end
 end
-class Post < ActiveRecord::Base
-  belongs_to :user
+
+module Antilaconia::Controllers
+  class Index
+    def get
+      @posts = Post.all
+      render :index
+    end
+  end
+end
+
+module Antilaconia::Views
+  def index
+    @posts.each do |post|
+      h1 post.title
+      div.content! { post.body }
+    end
+  end
 end
 
 ######################################################################
-
-u = User.new
-u.name = "wobblybob"
-u.save
-
-p1 = Post.new
-p1.user = u
-p1.text = "Hello world!"
-p1.save
-
-p1 = Post.new
-p1.user = u
-p1.text = "Hello world again!"
-p1.save
-
+Rack::Handler::CGI.run(Antilaconia) if __FILE__ == $0
